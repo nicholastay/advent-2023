eb69da3e8c908b4581db4d4612f3e0bc9d80d5aa
Nicholas Tay <nick@windblume.net>
2023-12-07 22:16:04 +1100

    Add day04 part01

---
+++ day04/day04.ml
@@ -0,0 +1,34 @@
+open Base
+
+type card = {
+  winning: int list;
+  numbers: int list;
+}
+
+let parse_numbers s =
+  String.strip s
+  |> String.split ~on:' ' |> List.filter ~f:(Fn.non String.is_empty)
+  |> List.map ~f:Int.of_string
+
+(* e.g. Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53 *)
+let split_card_line s =
+  String.lsplit2 s ~on:':' |> Option.value_exn |> snd |> String.lstrip
+  |> String.lsplit2 ~on:'|' |> Option.value_exn
+
+let parse_card s =
+  let (w,n) = split_card_line s in
+  {winning = parse_numbers w; numbers = parse_numbers n}
+
+let count_winning card =
+  List.cartesian_product card.winning card.numbers
+  |> List.count ~f:(fun (x,y) -> Int.equal x y)
+
+let () =
+  let argv = Sys.get_argv () in
+  let filename = argv.(1) in
+  let input = In_channel.with_open_bin filename In_channel.input_all |> String.strip in
+  String.split_lines input
+  |> List.map ~f:parse_card
+  |> List.map ~f:count_winning |> List.filter ~f:(Fn.non @@ Int.equal 0)
+  |> List.sum (module Int) ~f:(Fn.compose (Int.pow 2) @@ Fn.flip (-) 1)
+  |> Stdlib.print_int |> Stdlib.print_newline
